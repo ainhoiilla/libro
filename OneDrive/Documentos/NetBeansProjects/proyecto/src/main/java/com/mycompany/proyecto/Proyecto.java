69faf8ab29ceae940859377cd3e917cd16c012fb
Ainhoa <ainhoann66@gmail.com>
2025-03-04 12:33:45 +0100

    fin

---
+++ OneDrive/Documentos/NetBeansProjects/proyecto/src/main/java/com/mycompany/proyecto/Proyecto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+/**
+ *
+ * @author ainho
+ */
+public class Proyecto {
+    public static void menu(){
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+    }
+}
